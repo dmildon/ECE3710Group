91c84ab73cec15674e5a4ff6e5b32735cb36a782
Ahudz <Alexmhudson@gmail.com>
2021-09-09 17:40:28 -0600

    added ALU wrapper test bench
    updated ALU wrapper

--- ALU_wrapper.v
+++ ALU_wrapper.v
@@ -1,4 +1,4 @@
-module ALU_wrapper (clk, data_input, ld_op_code, ld_src, ld_dest, Flags, Out);
+module ALU_wrapper (clk, data_input, ld_op_code, ld_src, ld_dest, Flags, Out, out1, out2, out3, out4);
 
 	input clk; 
 	input [9:0] data_input;
@@ -9,7 +9,11 @@ module ALU_wrapper (clk, data_input, ld_op_code, ld_src, ld_dest, Flags, Out);
 	reg [15:0] Rsrc, Rdest;
 	reg [4:0] OpCode;
 	output [4:0] Flags;
-	output [15:0] Out;
+	output reg [15:0] Out;
+	output [6:0]out1;
+	output [6:0]out2;
+	output [6:0]out3;
+	output [6:0]out4;
 	
 	ALU myALU(
 		.Rsrc(Rsrc), 
@@ -18,6 +22,12 @@ module ALU_wrapper (clk, data_input, ld_op_code, ld_src, ld_dest, Flags, Out);
 		.Flags(Flags), 
 		.Out(Out)
 	); 
+	
+	hexTo7Seg seg1(
+		.x(data_input[3:0]),
+		.z(out1)
+	
+	);
 
 	//Register for opcode
 	always@(negedge(ld_op_code)) begin
@@ -47,4 +57,47 @@ module ALU_wrapper (clk, data_input, ld_op_code, ld_src, ld_dest, Flags, Out);
 	end
 	
 
+endmodule 
+
+module hexTo7Seg(
+		input [3:0]x,
+		output reg [6:0]z
+		);
+always @*
+case(x)
+	4'b0000 :			//Hexadecimal 0
+	z = ~7'b0111111;
+   4'b0001 :			//Hexadecimal 1
+	z = ~7'b0000110;
+   4'b0010 :			//Hexadecimal 2
+	z = ~7'b1011011;
+   4'b0011 : 			//Hexadecimal 3
+	z = ~7'b1001111;
+   4'b0100 : 			//Hexadecimal 4
+	z = ~7'b1100110;
+   4'b0101 : 			//Hexadecimal 5
+	z = ~7'b1101101;
+   4'b0110 : 			//Hexadecimal 6
+	z = ~7'b1111101;
+   4'b0111 :			//Hexadecimal 7
+	z = ~7'b0000111;
+   4'b1000 : 			//Hexadecimal 8
+	z = ~7'b1111111;
+   4'b1001 : 			//Hexadecimal 9
+	z = ~7'b1100111;
+	4'b1010 : 			//Hexadecimal A
+	z = ~7'b1110111;
+	4'b1011 : 			//Hexadecimal B
+	z = ~7'b1111100;
+	4'b1100 : 			//Hexadecimal C
+	z = ~7'b1011000;
+	4'b1101 : 			//Hexadecimal D
+	z = ~7'b1011110;
+	4'b1110 : 			//Hexadecimal E
+	z = ~7'b1111001;
+	4'b1111 : 			//Hexadecimal F	
+	z = ~7'b1110001; 
+   default :
+	z = ~7'b0000000;
+endcase
 endmodule 
