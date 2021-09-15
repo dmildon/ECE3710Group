f73a9b862f796fd42923c3a469bcee7730980941
dmildon <dawmildon@gmail.com>
2021-09-15 17:21:35 -0600

    Created RegFile.v and added top module and register module

--- RegFile/RegFile.v
+++ RegFile/RegFile.v
@@ -3,3 +3,21 @@ module RegFile ();
 endmodule 
 
 
+module Register(in, clk, en, rst, out);
+	input [15:0] in;
+	input clk, en, rst;
+	
+	output reg [15:0] out;
+	
+	always @(negedge rst, posedge clk) begin
+		if (~rst)
+			out <= 16'b0;
+		
+		else begin
+			if (en)
+				out <= X;
+			else
+				out <= out;
+		end
+	end
+endmodule 
