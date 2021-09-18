83972be9362cfc8f510f073c6000b04780495a80
u1073316@eng.utah.edu <u1073316@eng.utah.edu>
2021-09-18 13:14:57 -0600

    Updates.

--- RegFile/RegFile_Alu.v
+++ RegFile/RegFile_Alu.v
@@ -1,15 +1,15 @@
-module RegFile_Alu (RdestRegLoc, RsrcRegLoc, Clk, En, Rst, Imm,Imm_s, OpCode, TriBuffOut, Flags);
+module RegFile_Alu (RdestRegLoc, RsrcRegLoc, Clk, En, Rst, Imm,Imm_s, OpCode, AluOutput, RdestOut, Flags);
 
 	input [3:0] RdestRegLoc, RsrcRegLoc;
 	input Clk, En, Rst, Imm_s; //Imm_s is 1 when we want to use the Imm value, else its 0 if we want to use RegSrc
 	input [15:0] Imm; //the 16 bit immediate value
 	
-	inout [15:0] TriBuffOut; //feeds in to Reg load and is outputed from the ALU
+	output [15:0] AluOutput, RdestOut; //feeds in to Reg load and is outputed from the ALU
 	
 	input [4:0] OpCode;
 	output [4:0] Flags;
 	
-	wire [15:0] RdestOut, RsrcOut, AluOutput;
+	wire [15:0] RdestOut, RsrcOut;
 	reg [15:0] RsrcAlu;
 	
 	RegFile myReg(
@@ -18,7 +18,7 @@ module RegFile_Alu (RdestRegLoc, RsrcRegLoc, Clk, En, Rst, Imm,Imm_s, OpCode, Tr
 		.Clk(clk), 
 		.En(En), 
 		.Rst(Rst), 
-		.Load(TriBuffOut), 
+		.Load(AluOutput), 
 		.RdestOut(RdestOut), 
 		.RsrcOut(RscrcOut)
 	);
--- RegFile/tb_RegFile_Alu.v
+++ RegFile/tb_RegFile_Alu.v
@@ -1,3 +1,63 @@
-module tb_RegFile_Alu ();
+`timescale 1ps/1ps
 
+module tb_RegFile_Alu ();
+	reg [3:0] RdestRegLoc, RsrcRegLoc;
+	reg Clk, En, Rst, Imm_s;
+	reg [15:0] Imm;
+	reg [4:0] OpCode;
+	
+	wire [15:0] AluOutput, RdestOut; 
+	wire [4:0] Flags;
+	
+//	integer i, j;
+	
+	RegFile_Alu uut(
+		.RdestRegLoc(RdestRegLoc),
+		.RsrcRegLoc(RsrcRegLoc), 
+		.Clk(Clk), 
+		.En(En), 
+		.Rst(Rst), 
+		.Imm(Imm),
+		.Imm_s(Imm_s), 
+		.OpCode(OpCode), 
+		.AluOutput(AluOutput), 
+		.RdestOut(RdestOut),
+		.Flags(Flags)
+	);
+ 
+	initial begin
+		
+		$display("Starting integrated testbench");
+		
+		$display("");
+		
+		Clk = 0;
+		Rst = 1;
+		En = 0;
+		#5;
+		Rst = 0;
+		#5;
+		Rst = 1;
+		RdestRegLoc = 4'b0000;
+		#5;
+		$display("Rdest = %d", RdestOut);
+		#5;
+		
+		OpCode = 4'b0000;
+		Imm_s = 1;
+		Imm = 5;
+		
+		
+		
+		En =  1;
+		
+		#5;
+		$display("Rdest = %d", RdestOut);
+		En = 0;
+		#5;
+		
+		$display("Rdest = %d", RdestOut);
+	end
+	
+	always #5 Clk = ~Clk;
 endmodule
