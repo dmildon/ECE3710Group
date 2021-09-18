a82c5462ce3fefc44d5ea3542d30eaaeef706be6
dmildon <dawmildon@gmail.com>
2021-09-18 12:02:37 -0600

    Added TriBuff after ALU

--- RegFile/RegFile_Alu.v
+++ RegFile/RegFile_Alu.v
@@ -1,15 +1,15 @@
-module RegFile_Alu (RdestRegLoc, RsrcRegLoc, Clk, En, Rst, Imm,Imm_s, OpCode, AluOutput, Flags);
+module RegFile_Alu (RdestRegLoc, RsrcRegLoc, Clk, En, Rst, Imm,Imm_s, OpCode, TriBuffOut, Flags);
 
 	input [3:0] RdestRegLoc, RsrcRegLoc;
 	input Clk, En, Rst, Imm_s; //Imm_s is 1 when we want to use the Imm value, else its 0 if we want to use RegSrc
 	input [15:0] Imm; //the 16 bit immediate value
 	
-	inout [15:0] AluOutput; //feeds in to Reg load and is outputed from the ALU
+	inout [15:0] TriBuffOut; //feeds in to Reg load and is outputed from the ALU
 	
 	input [4:0] OpCode;
 	output [4:0] Flags;
 	
-	wire [15:0] RdestOut, RsrcOut;
+	wire [15:0] RdestOut, RsrcOut, AluOutput;
 	reg [15:0] RsrcAlu;
 	
 	RegFile myReg(
@@ -18,7 +18,7 @@ module RegFile_Alu (RdestRegLoc, RsrcRegLoc, Clk, En, Rst, Imm,Imm_s, OpCode, Al
 		.Clk(clk), 
 		.En(En), 
 		.Rst(Rst), 
-		.Load(AluOutput), 
+		.Load(TriBuffOut), 
 		.RdestOut(RdestOut), 
 		.RsrcOut(RscrcOut)
 	);
@@ -28,8 +28,14 @@ module RegFile_Alu (RdestRegLoc, RsrcRegLoc, Clk, En, Rst, Imm,Imm_s, OpCode, Al
 		.Rdest(RdestOut), 
 		.OpCode(OpCode), 
 		.Flags(Flags), 
-		.Out(aluOutput)
+		.Out(AluOutput)
 	); 
+	
+	TriStateBuff myBuff (
+		.in(AluOutput),
+		.en(Clk),
+		.out(TriBuffOut)
+	);
 	
 	always @(Imm_s) begin
 		if (~Imm_s)
@@ -40,4 +46,17 @@ module RegFile_Alu (RdestRegLoc, RsrcRegLoc, Clk, En, Rst, Imm,Imm_s, OpCode, Al
 		end
 	end
 	
+endmodule 
+
+
+module TriStateBuff (in, en, out);
+
+	input [15:0] in;
+	input en;
+   output reg [15:0] out;
+
+	always @(*) begin
+		out <= (en ? in : 'bz);
+	end
+
 endmodule 
